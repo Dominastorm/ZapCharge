249940edce6bd0feee3457dc2dfb0866a165eeba
Dominastorm <dchawla228@gmail.com>
2023-06-17 11:10:44 +0530

    Add mobile support

--- st_run.py
+++ st_run.py
@@ -1,5 +1,8 @@
 import streamlit as st
+
 from streamlit_option_menu import option_menu
+from tkinter import * 
+from tkinter.ttk import *
 
 from st_pages import chargers_by_city_view, chargers_by_location_view, user_requested_chargers_view, display_heatmap_info
 
@@ -11,13 +14,21 @@ page_config = st.set_page_config(
 st_style = """<style> footer {visibility: hidden;} </style>"""
 st.markdown(st_style, unsafe_allow_html=True)
 
+root = Tk()
+width = root.winfo_screenwidth()
+ 
+if width < 1000:
+    orientation = "vertical"
+else:
+    orientation = "horizontal"
+
 selected = option_menu(
     menu_title="ZapCharge",
     menu_icon="lightning-charge-fill",
     options=["Chargers by City", "Chargers by Location", "User Requested Chargers", "Traffic Heatmap"],
     icons=["ev-station-fill", "pin-map-fill",  "people-fill", "stoplights-fill"], # icons from https://icons.getbootstrap.com/
     default_index=0,
-    orientation="horizontal",
+    orientation=orientation
 )
 
 if selected == "Chargers by City":
